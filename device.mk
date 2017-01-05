2c10ab33104ec2001355c4905e85227099b9c5ee
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-01-06 02:15:35 +0800

    mocha: Fix WiFi connection in sleep mode
    * Adjust init files for use proprietary wpa_supplicant from miui 6.12.29
    
    Change-Id: I8ea6365764d5ebbc8b5167dfeb31c8a093f18a94

--- device.mk
+++ device.mk
@@ -130,7 +130,6 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 PRODUCT_PACKAGES += \
     hostapd \
-    wpa_supplicant \
     wpa_supplicant.conf
 
 # Light
