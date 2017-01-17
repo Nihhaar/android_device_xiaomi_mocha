c0344d9c99ac2de8132de9ddf24b3bb2093e0202
Nihhaar <nihhaar@gmail.com>
2017-01-17 04:41:31 +0000

    Fix Liblights for controlling button_brightness and screen_brightness

--- liblights/lights.c
+++ liblights/lights.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2014-2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,37 +14,59 @@
  * limitations under the License.
  */
 
+
+//#define LOG_NDEBUG 0
 #define LOG_TAG "lights"
 
 #include <cutils/log.h>
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 
-#include <sys/ioctl.h>
 #include <sys/types.h>
 
 #include <hardware/lights.h>
-#include <hardware/hardware.h>
 
+/******************************************************************************/
+
+static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
-static int write_int(char const *path, int value)
+const char *const LCD_FILE
+        = "/sys/class/leds/lcd-backlight/brightness";
+
+const char *const BUTTONS_FILE
+        = "/sys/class/leds/button-backlight/brightness";
+
+/**
+ * device methods
+ */
+
+void init_globals(void)
+{
+    // init the mutex
+    pthread_mutex_init(&g_lock, NULL);
+}
+
+static int write_int(char const* path, int value)
 {
     int fd;
-    static int already_warned = -1;
+    static int already_warned = 0;
+
     fd = open(path, O_RDWR);
     if (fd >= 0) {
         char buffer[20];
-        int bytes = sprintf(buffer, "%d\n", value);
-        int amt = write(fd, buffer, bytes);
+        int bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
+        ssize_t amt = write(fd, buffer, (size_t)bytes);
         close(fd);
         return amt == -1 ? -errno : 0;
     } else {
-        if (already_warned == -1) {
+        if (already_warned == 0) {
             ALOGE("write_int failed to open %s\n", path);
             already_warned = 1;
         }
@@ -52,23 +74,61 @@ static int write_int(char const *path, int value)
     }
 }
 
-static int rgb_to_brightness(struct light_state_t const *state)
+static int is_lit(struct light_state_t const* state)
+{
+    return state->color & 0x00ffffff;
+}
+
+static int rgb_to_brightness(const struct light_state_t *state)
 {
     int color = state->color & 0x00ffffff;
-    return ((77 * ((color >> 16) & 0x00ff))
-        + (150 * ((color >> 8) & 0x00ff)) +
-        (29 * (color & 0x00ff))) >> 8;
+    return ((77 * ((color >> 16) & 0xff))
+            + (150 * ((color >> 8) & 0xff))
+            + (29 * (color & 0xff))) >> 8;
+}
+
+static int convert_ms(int ms)
+{
+    int ms_out = 3;
+    if (ms <= 250) {
+        ms_out = 1;
+    } else if (ms <=500 ) {
+        ms_out = 2;
+    } else if (ms <=1000) {
+        ms_out = 3;
+    } else if (ms <=2500) {
+        ms_out = 4;
+    } else if (ms <=5000) {
+        ms_out = 5;
+    }
+    return ms_out;
 }
 
 static int set_light_backlight(struct light_device_t *dev,
-                   struct light_state_t const *state)
+        const struct light_state_t *state)
 {
     int err = 0;
     int brightness = rgb_to_brightness(state);
 
     pthread_mutex_lock(&g_lock);
-    err = write_int("/sys/class/backlight/pwm-backlight/brightness",
-            brightness);
+
+    err = write_int(LCD_FILE, brightness);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return err;
+}
+
+static int set_light_buttons(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    int err = 0;
+    int brightness = rgb_to_brightness(state);
+
+    pthread_mutex_lock(&g_lock);
+
+    err = write_int(BUTTONS_FILE, brightness);
+
     pthread_mutex_unlock(&g_lock);
 
     return err;
@@ -77,56 +137,60 @@ static int set_light_backlight(struct light_device_t *dev,
 /** Close the lights device */
 static int close_lights(struct light_device_t *dev)
 {
-    if (dev)
+    if (dev) {
         free(dev);
+    }
     return 0;
 }
 
-/** Open a new instance of a lights device using name */
-static int open_lights(const struct hw_module_t *module, char const *name,
-               struct hw_device_t **device)
-{
-    pthread_t lighting_poll_thread;
+/******************************************************************************/
 
-    int (*set_light) (struct light_device_t *dev,
-              struct light_state_t const *state);
+/**
+ * module methods
+ */
+
+/** Open a new instance of a lights device using name */
+static int open_lights(const struct hw_module_t *module, const char *name,
+        struct hw_device_t **device)
+{
+    int (*set_light)(struct light_device_t *dev,
+            const struct light_state_t *state);
 
     if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
         set_light = set_light_backlight;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
     else
         return -EINVAL;
 
-    pthread_mutex_init(&g_lock, NULL);
+    pthread_once(&g_init, init_globals);
 
     struct light_device_t *dev = malloc(sizeof(struct light_device_t));
     memset(dev, 0, sizeof(*dev));
 
     dev->common.tag = HARDWARE_DEVICE_TAG;
     dev->common.version = 0;
-    dev->common.module = (struct hw_module_t *)module;
-    dev->common.close = (int (*)(struct hw_device_t *))close_lights;
+    dev->common.module = (struct hw_module_t*)module;
+    dev->common.close = (int (*)(struct hw_device_t*))close_lights;
     dev->set_light = set_light;
 
-    *device = (struct hw_device_t *)dev;
-
+    *device = (struct hw_device_t*)dev;
     return 0;
 }
 
-static struct hw_module_methods_t lights_methods =
-{
+static struct hw_module_methods_t lights_module_methods = {
     .open =  open_lights,
 };
 
 /*
- * The backlight Module
+ * The lights Module
  */
-struct hw_module_t HAL_MODULE_INFO_SYM =
-{
+struct hw_module_t HAL_MODULE_INFO_SYM = {
     .tag = HARDWARE_MODULE_TAG,
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "NVIDIA Ardbeg lights module",
-    .author = "NVIDIA",
-    .methods = &lights_methods,
+    .name = "Lights Module",
+    .author = "The CyanogenMod Project",
+    .methods = &lights_module_methods,
 };
