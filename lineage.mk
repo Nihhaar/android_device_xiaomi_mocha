9dbfbfc1dd73b61e1d546b9e0c4fe269a93d0efa
Nihhaar <nihhaar@gmail.com>
2017-02-08 04:44:38 +0530

    Move to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mocha
-PRODUCT_NAME := cm_mocha
+PRODUCT_NAME := lineage_mocha
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Pad
 PRODUCT_MANUFACTURER := Xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_mocha-userdebug
+add_lunch_combo lineage_mocha-userdebug
