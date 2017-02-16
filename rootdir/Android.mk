6a9b5ed4783c3a84dfdfb6ce12591bab687106b3
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-02-16 20:32:10 +0800

    mocha: delete obsole init file and drop some legacy services

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -81,14 +81,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.tn8_emmc.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.tn8_emmc.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.ussrd.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
